9097aab77e029edc084ef4cb06d35debf59369fd
muhammadjon <xamidovmuhammadjon748@gmail.com>
2024-06-29 20:40:34 +0500

    some changes

--- book/views.py
+++ book/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
 
+def home(request):
+    return render(request, 'home.html')
+
+def login(request):
+    return render(request, 'login.html')
+
+def register(request):
+    return render(request, 'register.html')
+
+def books(request):
+    return render(request, 'books.html')
+
+def authors(request):
+    return render(request, 'authors.html')
+
 # Create your views here.
--- first_project/settings.py
+++ first_project/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-*ogqqua$qim&ncu0-l-d_9#i&*(!&p+g2&-%6#77h158$$*vr$'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -40,6 +40,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # my apps
+    'book',
 ]
 
 MIDDLEWARE = [
