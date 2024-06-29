9fad2b0c9ccfd885153dd322a8bff1b274b66d14
muhammadjon <xamidovmuhammadjon748@gmail.com>
2024-06-29 21:35:34 +0500

    some changes

--- book/models.py
+++ book/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Book(models.model):
+    title = models.CharField(max_length=100, null=False)
+    description = models.TextField()
+    author = models.CharField(max_length=50, null=False)
+    count = models.PositiveIntegerField()
+    price = models.FloatField()
+    create_date = models.DateTimeField(auto_now_add=True)
