25110caebe48228ce66513f36916a2b9f43da716
muhammadjon <xamidovmuhammadjon748@gmail.com>
2024-07-01 18:49:19 +0500

    some changes

--- edu_center/admin.py
+++ edu_center/admin.py
@@ -1,5 +1,16 @@
 from django.contrib import admin
 from .models import Book, Authors
 # Register your models here.
+# book = Book[1]
+# author = Authors[1]
 
-admin.site.register(Book) 
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title',)
+    # search_fields = ('title', 'author__name')
+    # list_filter = ('create_date', 'author')
+
+class AuthorsAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name')
+
+admin.site.register(Book, BookAdmin)
+admin.site.register(Authors, AuthorsAdmin)
--- edu_center/models.py
+++ edu_center/models.py
@@ -2,16 +2,6 @@ from typing import Any
 from django.db import models
 
 # Create your models here.
-class Book(models.Model):
-    title = models.CharField(max_length=100, null=False)
-    description = models.TextField()
-    author = models.CharField(max_length=50, null=False)
-    count = models.PositiveIntegerField()
-    price = models.FloatField()
-    create_date = models.DateTimeField(auto_now_add=True)
-    def __str__(self) -> str:
-        return self.title, self.description, self.author, self.count, self.price, self.create_date
-
 class Authors(models.Model):
     first_name = models.CharField(max_length=100, null=False)
     last_name = models.CharField(max_length=50, null=False)
@@ -19,4 +9,14 @@ class Authors(models.Model):
     birth_date = models.DateTimeField()
     create_date = models.DateTimeField(auto_now_add=True)
     def __str__(self) -> str:
-        return self.first_name, self.last_name, self.autobiorophy, self.birth_date, self.create_date
+        return f"{[self.first_name, self.last_name, self.autobiorophy, self.birth_date, self.create_date]}"
+
+class Book(models.Model):
+    title = models.CharField(max_length=100, null=False)
+    description = models.TextField()
+    author = models.ForeignKey(Authors, on_delete=models.CASCADE)
+    count = models.PositiveIntegerField()
+    price = models.FloatField()
+    create_date = models.DateTimeField(auto_now_add=True)
+    def __str__(self) -> str:
+        return f"{[self.title, self.description, self.author, self.count, self.price, self.create_date]}"
