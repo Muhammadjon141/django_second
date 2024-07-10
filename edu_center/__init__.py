3d7412288f449eb5c0b4ee2e9009eb90a8b7e624
muhammadjon <xamidovmuhammadjon748@gmail.com>
2024-07-10 20:20:05 +0500

    some changes

--- edu_center/__init__.py
+++
@@ -1,1 +0,0 @@
-default_app_config = 'new_name.apps.NewNameConfig'
--- edu_center/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class EduCenterConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'edu_center'
